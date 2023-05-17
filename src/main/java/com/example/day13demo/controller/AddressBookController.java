943f2eecd9fed97661e182f62657c2dd4b1a7a43
Yenleng <yenlengggggg@gmail.com>
2023-05-17 11:41:42 +0800

    added thymeleaf form validation

--- src/main/java/com/example/day13demo/controller/AddressBookController.java
+++ src/main/java/com/example/day13demo/controller/AddressBookController.java
@@ -2,11 +2,18 @@ package com.example.day13demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.MultiValueMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.day13demo.model.Contact;
+
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping(path = "/")
@@ -18,13 +25,29 @@ public class AddressBookController {
         model.addAttribute("contact", new Contact());
         return "addressBook";
     }
-
     //to save contact info
-    @PostMapping("/contact")
-    public String saveAddressBook(Contact contact, Model model){
+    @PostMapping(consumes = "application/x-www-form-urlencoded", path = "/contact")
+    public String saveAddressBook(@Valid Contact contact, BindingResult bindingResult, Model model){
+     
+                
+        
         System.out.println("Name: " + contact.getName());
         System.out.println("Email: " + contact.getEmail());
         System.out.println("Phone Number: " + contact.getPhoneNumber());
+
+        if(bindingResult.hasErrors()){
+            //System.out.println("Error count ---> " + result.getErrorCount());
+            System.out.println(bindingResult.toString());
+            return "addressBook";
+        }
+
+           // String name = form.getFirst("name");
+        // String email = form.getFirst("email");
+        // String phone = form.getFirst("phoneNumber");
+
+        // System.out.println("name: " + name); 
+        // System.out.println("email: " + email);
+        // System.out.println("phone: " + phone);
         return "addressBook";
     }
 
--- src/main/java/com/example/day13demo/model/Contact.java
+++ src/main/java/com/example/day13demo/model/Contact.java
@@ -1,8 +1,20 @@
 package com.example.day13demo.model;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 public class Contact {
+    @NotNull(message="Name cannot be empty")
+    @Size(min = 3, max = 15, message ="Name should be between 3-15 characters")
     private String name;
+
+    @NotEmpty(message="Email cannot be empty")
+    @Email(message="Invalid Email")
     private String email;
+
+    @Size(min = 8, message ="Invalid phone number")
     private String phoneNumber;
     
     
